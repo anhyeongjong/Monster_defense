a019b46e49e847fd1f7bc400b121fee5649d1299
anhyeongjong <65288275+anhyeongjong@users.noreply.github.com>
2021-12-01 18:52:52 +0900

    Player_controller
    - 캐릭터 오른손 왼손 상호 작용 버튼

---
+++ Assets/Scripts/Player_controller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+public class Player_controller : MonoBehaviour
+{
+    // 핸드 타입
+    private SteamVR_Input_Sources right_Hand;
+    private SteamVR_Input_Sources left_Hand;
+
+    // 컨트롤러 버튼 타입
+    public SteamVR_Action_Boolean function_Key;
+
+    void Start()
+    {
+        right_Hand = SteamVR_Input_Sources.RightHand;
+        left_Hand = SteamVR_Input_Sources.LeftHand;
+    }
+
+    void Update()
+    {
+        // right_function 클릭 시 
+        if (function_Key.GetState(right_Hand))
+        {
+            // 총 발사
+            Debug.Log("shot");
+        }
+
+        // left_function 클릭 시 
+        if (function_Key.GetLastStateDown(left_Hand))
+        {
+            // 총(스킬) 체인지
+            Debug.Log("change");
+        }
+    }
+}
